b52235934b6cb7d09b579b61a424c7cafe1c6c5a
Erin Lim <erinlim@utexas.edu>
2021-08-02 04:08:09 -0500

    edits

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # E-Commerce
+
+https://drive.google.com/file/d/1uBuIxFkUCrSrn8qRRU-vab-CgIa4gPK6/view
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,6 +6,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+    {
+      model: Category,
+      attributes: ['category_name'],
+  },
+  {
+      model: Tag,
+      attributes: ['tag_name'],
+  }
+]
+  })
+    .then((products) => res.json(products))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
   // be sure to include its associated Category and Tag data
 });
 
@@ -13,6 +30,24 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+        id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+        {
+            model: Category,
+            attributes: ['category_name'],
+        },
+        {
+            model: Tag,
+            attributes: ['tag_name'],
+        }
+    ]
+}).then((oneProduct) => {
+    res.json(oneProduct)
+})
 });
 
 // create new product
@@ -91,6 +126,11 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    }
+  })
 });
 
 module.exports = router;
